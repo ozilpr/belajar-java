29b11bc0450991a1b4133ac82aaac3e58c15031a
ozilpr <ozilp48@gmail.com>
2024-06-04 19:52:50 +0700

    array

--- src/com/dicoding/javafundamental/array/InisiasiArray.java
+++ src/com/dicoding/javafundamental/array/InisiasiArray.java
@@ -4,13 +4,15 @@ public class InisiasiArray {
     public static void main(String[] args) {
 //        int[] arrInt = new int[]{1, 2, 3, 4, 5, 6};
 
-        int[] arrInt = new int[6];
-        arrInt[0] = 1;
-        arrInt[1] = 2;
-        arrInt[2] = 3;
-        arrInt[3] = 4;
-        arrInt[4] = 5;
-        arrInt[5] = 6;
+//        int[] arrInt = new int[6];
+//        arrInt[0] = 1;
+//        arrInt[1] = 2;
+//        arrInt[2] = 3;
+//        arrInt[3] = 4;
+//        arrInt[4] = 5;
+//        arrInt[5] = 6;
+
+        int[] arrInt = {1, 2, 3, 4, 5, 6};
 
         System.out.println(arrInt[0]);
         System.out.println(arrInt[1]);
